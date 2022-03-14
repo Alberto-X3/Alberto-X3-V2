35c2dfa4a8f671886ed81ac144ca1c2c10ed522e
AlbertUnruh <AlbertUnruh@pm.me>
2022-03-14 19:57:25 +0100

    fix `__init__.py`
    - add `.stats`

--- AlbertoX3/__init__.py
+++ AlbertoX3/__init__.py
@@ -5,15 +5,16 @@ __copyright__ = f"Copyright 2021-present (c) {__author__} - Alberto-X3"
 __url__ = "https://github.com/Alberto-X3/Alberto-X3-V2"
 
 
+from .adis_snek import *
 from .aio import *
 from .colors import *
 from .config import *
 from .database import *
-from .adis_snek import *
 from .enum import *
 from .environment import *
 from .permissions import *
 from .settings import *
+from .stats import *
 from .translations import *
 from .types import *
 from .utils import *
