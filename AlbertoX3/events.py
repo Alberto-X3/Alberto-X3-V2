db7d41901c8e9c61e321646b16e14914c4b6cc58
AlbertUnruh <AlbertUnruh@pm.me>
2022-04-21 23:07:33 +0200

    update event processing
    - add remaining listeners

--- AlbertoX3/events.py
+++ AlbertoX3/events.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 __all__ = ("apply_block_events_adapter",)
 
 
+from asyncio import gather
 from sqlalchemy import Column, BigInteger
 from typing import TYPE_CHECKING
 
@@ -12,13 +13,15 @@ from dis_snek.client.utils import TTLCache
 
 from AlbertUnruhUtils.utils.logger import get_logger
 
+from .config import Config
 from .database import Base, db, db_context
 from .stats import DailyStatsModel
+from . import listener
 
 
 if TYPE_CHECKING:
     from dis_snek import Snake
-    from typing import Callable, Coroutine
+    from typing import Callable, Coroutine, Set
 
 
 logger = get_logger(__name__.split(".")[-1], level=None, add_handler=False)
@@ -73,34 +76,70 @@ class BlockEventsAdapter:
         self.processor = processor
 
     async def __call__(self, event: RawGatewayEvent):
-        collected = set()
+        collected_ids: Set[int] = set()
+        event_listeners: Set[listener.Channel] = set()
 
         # data[user_id]
         if (tmp := event.data.get("user_id")) is not None:
-            collected.add(tmp)
+            collected_ids.add(int(tmp))
 
         # data[author_id]
         if (tmp := event.data.get("author_id")) is not None:
-            collected.add(tmp)
+            collected_ids.add(int(tmp))
 
         # data[user][id]
         if (tmp := event.data.get("user")) is not None:
             if (tmp := tmp.get("id")) is not None:
-                collected.add(tmp)
+                collected_ids.add(int(tmp))
 
         # data[author][id]
         if (tmp := event.data.get("author")) is not None:
             if (tmp := tmp.get("id")) is not None:
-                collected.add(tmp)
+                collected_ids.add(int(tmp))
 
         async with db_context():
-            for id in collected:  # noqa
-                if await BlockedUserModel.is_blocked(int(id)):
-                    logger.debug(f"Blocked dispatching Event: {event.resolved_name}")
-                    return
+
             await DailyStatsModel.incr_events()  # sneak it into the new session ^^
 
-        await self.processor(event)
+            for id in collected_ids:  # noqa
+
+                # event by the owner
+                if id == Config.AUTHOR.discord_id:
+                    # the owner has the right to do everything ^^
+                    event_listeners.add(self.processor)  # type: ignore
+                # event by a contributor
+                if id in map(lambda x: x.discord_id, Config.CONTRIBUTORS):
+                    event_listeners.add(listener.event_contributor_event)
+
+                if await BlockedUserModel.is_blocked(id):
+                    logger.debug(f"Blocked dispatching Event: {event.resolved_name}")
+
+                    # message create  (due to moderation)
+                    if event.resolved_name == "raw_message_create":
+                        event_listeners.add(listener.blocked_event_message_create)
+                    # message update  (due to moderation)
+                    if event.resolved_name == "raw_message_update":
+                        event_listeners.add(listener.blocked_event_message_update)
+                    # message delete  (due to moderation)
+                    if event.resolved_name == "raw_message_delete":
+                        event_listeners.add(listener.blocked_event_message_delete)
+                    # guild member add  (due to nickname-managing/join message)
+                    if event.resolved_name == "raw_guild_member_add":
+                        event_listeners.add(listener.blocked_event_guild_member_add)
+                    # guild member update  (due to nickname-managing)
+                    if event.resolved_name == "raw_guild_member_update":
+                        event_listeners.add(listener.blocked_event_guild_member_update)
+                    # guild member remove  (due to nickname-managing/leave message)
+                    if event.resolved_name == "raw_guild_member_remove":
+                        event_listeners.add(listener.blocked_event_guild_member_remove)
+
+                    await gather(*[el(event) for el in event_listeners])
+
+                    return
+
+        event_listeners.add(self.processor)  # type: ignore
+
+        await gather(*[el(event) for el in event_listeners])
 
 
 def apply_block_events_adapter(bot: Snake):
